1e4f3713eda8172918cb20e734d3b8453e7644f3
sporwal98 <32335522+sporwal98@users.noreply.github.com>
2020-01-30 12:46:58 +0000

    Add files via upload

--- move.py
+++ move.py
@@ -1,60 +1,47 @@
 import ev3dev.ev3 as ev3
-#NOTE: NEEDS SOME TESTING AND EDITTING
-fr = ev3.LargeMotor('outA');
-fl = ev3.LargeMotor('outB');
-br = ev3.LargeMotor('outC');
-bl = ev3.LargeMotor('outD');
+
+fr = ev3.LargeMotor('outA');#Front Right wheel to port A
+fl = ev3.LargeMotor('outB');#Front Left wheel to port B
+br = ev3.LargeMotor('outC');#Back Right wheel to port C
+bl = ev3.LargeMotor('outD');#Back Left wheel to port D
+
 def forward(speed, time):
+    #Move robot forward at speed(tacho counts per second) for time(milliseconds)
     fr.run_timed(speed_sp = speed, time_sp = time);
     fl.run_timed(speed_sp = speed, time_sp = time);
     br.run_timed(speed_sp = -speed, time_sp = time);    
     bl.run_timed(speed_sp = -speed, time_sp = time);
     return
+
 def back(speed, time):
+    #Move robot back at speed(tacho counts per second) for time(milliseconds)
     forward(-speed,time)
     return
+
 def right(speed, time):
-    fr.run_timed(speed_sp = -speed, time_sp = time);
-    fl.run_timed(speed_sp = speed, time_sp = time);
-    br.run_timed(speed_sp = speed, time_sp = time);
-    bl.run_timed(speed_sp = -speed, time_sp = time);
-    return
-def left(speed, time):
-    right(-speed,time)
-    return
-def fright(speed, time):
-    fr.run_timed(speed_sp = 0, time_sp = time);
-    fl.run_timed(speed_sp = speed, time_sp = time);
-    br.run_timed(speed_sp = speed, time_sp = time);
-    bl.run_timed(speed_sp = 0, time_sp = time);
-    return
-def fleft(speed, time):
-    fr.run_timed(speed_sp = speed, time_sp = time);
-    fl.run_timed(speed_sp = 0, time_sp = time);
-    br.run_timed(speed_sp = 0, time_sp = time);
-    bl.run_timed(speed_sp = speed, time_sp = time);
-    return
-def bright(speed, time):
-    fleft(-speed,time)
-    return
-def bleft(speed, time):
-    fright(-speed,time)
-    return
-def rotr(speed,time):
+    #Move robot right at speed(tacho counts per second) for time(milliseconds)
     fr.run_timed(speed_sp = -speed, time_sp = time);
     fl.run_timed(speed_sp = speed, time_sp = time);
     br.run_timed(speed_sp = -speed, time_sp = time);
     bl.run_timed(speed_sp = speed, time_sp = time);
     return
+
+def left(speed, time):
+    #Move robot left at speed(tacho counts per second) for time(milliseconds)
+    right(-speed,time)
+    return
+
+def rotr(speed,time):
+    #Rotate robot clockwise(right) at speed(tacho counts per second) for time(milliseconds)
+    fr.run_timed(speed_sp = -speed, time_sp = time);
+    fl.run_timed(speed_sp = speed, time_sp = time);
+    br.run_timed(speed_sp = speed, time_sp = time);
+    bl.run_timed(speed_sp = -speed, time_sp = time);
+    return
+
 def rotl(speed,time):
+    #Rotate robot anticlockwise(left) at speed(tacho counts per second) for time(milliseconds)
     rotr(-speed,time)
     return
 
-def main():
-    forward(500,1000);
-    right(500,1000);
-    forward(-500,1000);
-    right(-500,1000);
-
-main()
 
