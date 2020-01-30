be320e45314b529525b620c507fb8b0020a4c3b7
AimeeRedbond <aimeeredbond@gmail.com>
2020-01-30 12:37:09 +0000

    starts ui for manual controls

---
+++ neat_ui/app/src/main/java/neat_ui/com/MainActivity.java
@@ -0,0 +1,41 @@
+package neat_ui.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.movement_controls);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                goToManualMovementActivity();
+
+            }
+
+        });
+    }
+
+    private void goToManualMovementActivity() {
+
+        Intent intent = new Intent(this, ManualMovementActivity.class);
+
+        startActivity(intent);
+
+    }
+}
---
+++ neat_ui/app/src/main/java/neat_ui/com/ManualMovementActivity.java
@@ -0,0 +1,14 @@
+package neat_ui.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualMovementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_movement);
+    }
+}
