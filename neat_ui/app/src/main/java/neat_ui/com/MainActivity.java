969e9a8b1c821c222664221320ef3d35d2b413a0
AimeeRedbond <aimeeredbond@gmail.com>
2020-02-01 12:43:58 +0000

    made button vibrate on click

--- neat_ui/app/src/main/java/neat_ui/com/MainActivity.java
+++ neat_ui/app/src/main/java/neat_ui/com/MainActivity.java
@@ -2,9 +2,11 @@ package neat_ui.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
 
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Vibrator vibe = (Vibrator) MainActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
+
 
         Button button = findViewById(R.id.manual_mode);
 
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View v) {
 
+                vibe.vibrate(100);
                 goToManualMovementActivity();
 
             }
