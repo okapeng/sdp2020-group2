f46b33b113ac44573db0c7b0079cf8f713729a5f
AimeeRedbond <aimeeredbond@gmail.com>
2020-02-01 14:08:11 +0000

    make manual controls vibrate when held

--- neat_ui/app/src/main/java/neat_ui/com/ManualMovementActivity.java
+++ neat_ui/app/src/main/java/neat_ui/com/ManualMovementActivity.java
@@ -2,7 +2,12 @@ package neat_ui.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ManualMovementActivity extends AppCompatActivity {
 
@@ -10,5 +15,92 @@ public class ManualMovementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_movement);
+
+        final Vibrator vibe = (Vibrator) ManualMovementActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
+
+        ImageButton button_up = findViewById(R.id.up);
+
+        button_up.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
+                    vibe.vibrate(300000000);
+                } else
+                if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+                    vibe.cancel();
+                }
+                return false;
+            }
+        });
+
+        ImageButton button_down = findViewById(R.id.down);
+
+        button_down.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
+                    vibe.vibrate(300000000);
+                } else
+                if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+                    vibe.cancel();
+                }
+                return false;
+            }
+        });
+
+        ImageButton button_left = findViewById(R.id.left);
+
+        button_left.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
+                    vibe.vibrate(300000000);
+                } else
+                if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+                    vibe.cancel();
+                }
+                return false;
+            }
+        });
+
+        ImageButton button_right = findViewById(R.id.right);
+
+        button_right.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
+                    vibe.vibrate(300000000);
+                } else
+                if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+                    vibe.cancel();
+                }
+                return false;
+            }
+        });
+
+        ImageButton button_rotate_r = findViewById(R.id.rotate_right);
+
+        button_rotate_r.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
+                    vibe.vibrate(300000000);
+                } else
+                if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+                    vibe.cancel();
+                }
+                return false;
+            }
+        });
+
+        ImageButton button_rotate_l = findViewById(R.id.rotate_left);
+
+        button_rotate_l.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
+                    vibe.vibrate(300000000);
+                } else
+                if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+                    vibe.cancel();
+                }
+                return false;
+            }
+        });
+
     }
 }
