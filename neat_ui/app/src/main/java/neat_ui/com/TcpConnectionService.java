c0ec83d3508b3160cca534b9a976aecbee5f50f0
AimeeRedbond <aimeeredbond@gmail.com>
2020-02-04 21:54:29 +0000

    update tcp connection and apply to buttons in manual mode

--- neat_ui/app/src/main/java/neat_ui/com/TcpConnectionService.java
+++ neat_ui/app/src/main/java/neat_ui/com/TcpConnectionService.java
@@ -1,52 +1,69 @@
 package neat_ui.com;
 
 
+import android.os.AsyncTask;
+
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class TcpConnectionService {
-    private final String hostAddr = "192.168.105.101";
-    private final int port = 4444;
+public class TcpConnectionService extends AsyncTask {
+    private String hostAddr = "192.168.105.101";
+    private int port = 4445;
 
     private Socket socket;
     private PrintWriter printWriter;
     private ExecutorService mExecutorService;
 
-//    public TcpConnectionService(String hostAddr, int port) {
-//        this.hostAddr = hostAddr;
-//        this.port = port;
-//    }
-    public TcpConnectionService() {
+    public TcpConnectionService(String hostAddr, int port) {
+        this.hostAddr = hostAddr;
+        this.port = port;
     }
 
-    public void connect() {
+    public void send(String msg) {
+        mExecutorService.execute(new SendService(socket, msg));
         try {
-            this.socket = new Socket(hostAddr, port);
-
-            mExecutorService = Executors.newCachedThreadPool();
+            socket.getOutputStream().flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void send(String msg) {
-        mExecutorService.execute(new SendService(msg));
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        try {
+            this.socket = new Socket(hostAddr, port);
+//            this.printWriter = new Psocket.getOutputStream());
+
+            mExecutorService = Executors.newCachedThreadPool();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private class SendService implements Runnable {
+        private Socket s;
         private String msg;
 
-        SendService(String msg) {
+        SendService(Socket s, String msg) {
+            this.s = s;
             this.msg = msg;
         }
 
         @Override
         public void run() {
-            printWriter.println(this.msg);
+            try {
+                OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream());
+                out.write(this.msg);
+                out.flush();
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
     }
