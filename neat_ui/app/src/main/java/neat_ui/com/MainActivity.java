939ad20507eb7924b571d67a53d2143d0e33840c
AimeeRedbond <aimeeredbond@gmail.com>
2020-02-04 21:54:29 +0000

    update tcp connection and apply to buttons in manual mode

--- neat_ui/app/src/main/java/neat_ui/com/MainActivity.java
+++ neat_ui/app/src/main/java/neat_ui/com/MainActivity.java
@@ -3,9 +3,6 @@ package neat_ui.com;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -14,25 +11,9 @@ import android.content.Intent;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.util.Set;
-import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
-    private final static int REQUEST_ENABLE_BT = 1;
-    private BluetoothDevice mmDevice;
-    private UUID deviceUUID;
-    static ConnectedThread mConnectedThread;
-    private static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private String deviceName = "ev3dev";
-    private final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    private BluetoothSocket mmSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,20 +29,7 @@ public class MainActivity extends AppCompatActivity {
         boolean isComing = false;
         boolean isConnected = false;
 
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(getApplicationContext(), "This device does not support Bluetooth",
-                    Toast.LENGTH_LONG).show();
-            return;
-        }
-
         checkWhetherConnected(isConnected);
-        if (mBluetoothAdapter != null && !mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new
-                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        pairDevices();
 
         final Button button_manual = findViewById(R.id.manual_mode);
 
@@ -156,145 +124,4 @@ public class MainActivity extends AppCompatActivity {
         DialogFragment newFragment = new ConnectionPopUp();
         newFragment.show(getSupportFragmentManager(), "connect pop-up");
     }
-
-    public void pairDevices() {
-        mmDevice = null;
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                if (device.getName().equals(deviceName)) {
-                    mmDevice = device;
-                    Toast.makeText(getApplicationContext(), "Paired with your N.E.A.T.",
-                            Toast.LENGTH_LONG).show();
-                    break;
-                }
-            }
-        }
-        if (mmDevice == null) {
-            Toast.makeText(getApplicationContext(), "Not paired with your N.E.A.T.",
-                    Toast.LENGTH_LONG).show();
-            return;
-            // perhaps add in ability to discover here
-        }
-
-        ConnectThread connect = new ConnectThread(mmDevice, mUUID);
-        connect.start();
-    }
-
-    public class ConnectThread extends Thread {
-
-        public ConnectThread(BluetoothDevice device, UUID uuid) {
-            mmDevice = device;
-            deviceUUID = uuid;
-        }
-
-        public void run(){
-            //cancelDiscovery just in case since it reduces likelihood of a successful connection
-            mBluetoothAdapter.cancelDiscovery();
-            //Creating the socket.
-            BluetoothSocket tmp = null;
-            try {
-                tmp = mmDevice.createRfcommSocketToServiceRecord(deviceUUID);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            mmSocket = tmp;
-            //socket created, try to connect
-            try {
-                mmSocket.connect();
-                System.out.println("Connected");
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Can't connect to socket");
-                try {
-                    mmSocket.close();
-                } catch (IOException e2) {
-                    System.out.println("socket did not close");
-                    e2.printStackTrace();
-                }
-                return;
-            }
-            //start a new thread for communications
-            connected(mmSocket);
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    private void connected(BluetoothSocket mmSocket) {
-        // Start the thread to manage the connection and perform transmissions
-        mConnectedThread = new ConnectedThread(mmSocket);
-        mConnectedThread.start();
-    }
-
-    public class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            try {
-                tmpIn = mmSocket.getInputStream();
-                tmpOut = mmSocket.getOutputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run(){
-            byte[] buffer = new byte[1024];  // buffer store for the stream
-
-            int bytes; // bytes returned from read()
-
-            // Keep listening to the InputStream until an exception occurs
-            while (true) {
-                // Read from the InputStream
-                try {
-                    bytes = mmInStream.read(buffer);
-                    String incomingMessage = new String(buffer, 0, bytes);
-                    //do something with this
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    break;
-                }
-            }
-        }
-
-
-        public void write(String message) {
-            byte[] bytes = message.getBytes(Charset.defaultCharset());
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        /* Call this from the main activity to shutdown the connection */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
--- neat_ui/app/src/main/java/neat_ui/com/ManualMovementActivity.java
+++ neat_ui/app/src/main/java/neat_ui/com/ManualMovementActivity.java
@@ -4,120 +4,238 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class ManualMovementActivity extends AppCompatActivity {
+    final TcpConnectionService tcp = new TcpConnectionService("192.168.105.101", 4445);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_movement);
 
+        //tcp.execute();
+
         final Vibrator vibe = (Vibrator) ManualMovementActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
 
         final ImageButton button_up = findViewById(R.id.up);
 
         button_up.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
-                    button_up.setBackgroundResource(R.drawable.pressed_green_button);
-                    vibe.vibrate(900000000);
-                    //MainActivity.mConnectedThread.write("move forward");
-                } else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
-                    button_up.setBackgroundResource(R.drawable.green_rounded_corner);
-                    vibe.cancel();
-                    //MainActivity.mConnectedThread.write("stop");
+
+            private Handler mHandler;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        button_up.setBackgroundResource(R.drawable.pressed_green_button);
+                        vibe.vibrate(900000000);
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 100);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        button_up.setBackgroundResource(R.drawable.green_rounded_corner);
+                        vibe.cancel();
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
                 }
                 return false;
             }
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    //tcp.send("forward");
+                    System.out.println("forward");
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+
         });
 
         final ImageButton button_down = findViewById(R.id.down);
 
         button_down.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
-                    button_down.setBackgroundResource(R.drawable.pressed_green_button);
-                    vibe.vibrate(900000000);
-                    //MainActivity.mConnectedThread.write("move backward");
-                } else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
-                    button_down.setBackgroundResource(R.drawable.green_rounded_corner);
-                    vibe.cancel();
-                    //MainActivity.mConnectedThread.write("stop");
+
+            private Handler mHandler;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        button_down.setBackgroundResource(R.drawable.pressed_green_button);
+                        vibe.vibrate(900000000);
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 100);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        button_down.setBackgroundResource(R.drawable.green_rounded_corner);
+                        vibe.cancel();
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
                 }
                 return false;
             }
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    //tcp.send("back");
+                    System.out.println("back");
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+
         });
 
         final ImageButton button_left = findViewById(R.id.left);
 
         button_left.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
-                    button_left.setBackgroundResource(R.drawable.pressed_green_button);
-                    vibe.vibrate(900000000);
-                    //MainActivity.mConnectedThread.write("move left");
-                } else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
-                    button_left.setBackgroundResource(R.drawable.green_rounded_corner);
-                    vibe.cancel();
-                    //MainActivity.mConnectedThread.write("stop");
+
+            private Handler mHandler;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        button_left.setBackgroundResource(R.drawable.pressed_green_button);
+                        vibe.vibrate(900000000);
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 100);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        button_left.setBackgroundResource(R.drawable.green_rounded_corner);
+                        vibe.cancel();
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
                 }
                 return false;
             }
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    //tcp.send("left");
+                    System.out.println("left");
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+
         });
 
         final ImageButton button_right = findViewById(R.id.right);
 
         button_right.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
-                    button_right.setBackgroundResource(R.drawable.pressed_green_button);
-                    vibe.vibrate(900000000);
-                    //MainActivity.mConnectedThread.write("move right");
-                } else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
-                    button_right.setBackgroundResource(R.drawable.green_rounded_corner);
-                    vibe.cancel();
-                    //MainActivity.mConnectedThread.write("stop");
+
+            private Handler mHandler;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        button_right.setBackgroundResource(R.drawable.pressed_green_button);
+                        vibe.vibrate(900000000);
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 100);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        button_right.setBackgroundResource(R.drawable.green_rounded_corner);
+                        vibe.cancel();
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
                 }
                 return false;
             }
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    //tcp.send("right");
+                    System.out.println("right");
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+
         });
 
         final ImageButton button_rotate_r = findViewById(R.id.rotate_right);
 
         button_rotate_r.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
-                    button_rotate_r.setBackgroundResource(R.drawable.pressed_yellow_circle);
-                    vibe.vibrate(900000000);
-                    //MainActivity.mConnectedThread.write("rotate right");
-                } else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
-                    button_rotate_r.setBackgroundResource(R.drawable.yellow_circle);
-                    vibe.cancel();
-                    //MainActivity.mConnectedThread.write("stop");
+
+            private Handler mHandler;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        button_rotate_r.setBackgroundResource(R.drawable.pressed_yellow_circle);
+                        vibe.vibrate(900000000);
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 100);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        button_rotate_r.setBackgroundResource(R.drawable.yellow_circle);
+                        vibe.cancel();
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
                 }
                 return false;
             }
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    //tcp.send("rotr");
+                    System.out.println("rotr");
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+
         });
 
         final ImageButton button_rotate_l = findViewById(R.id.rotate_left);
 
         button_rotate_l.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == android.view.MotionEvent.ACTION_DOWN ) {
-                    button_rotate_l.setBackgroundResource(R.drawable.pressed_yellow_circle);
-                    vibe.vibrate(900000000);
-                    //MainActivity.mConnectedThread.write("rotate left");
-                } else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
-                    button_rotate_l.setBackgroundResource(R.drawable.yellow_circle);
-                    vibe.cancel();
-                    //MainActivity.mConnectedThread.write("stop");
+
+            private Handler mHandler;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        button_rotate_l.setBackgroundResource(R.drawable.pressed_yellow_circle);
+                        vibe.vibrate(900000000);
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 100);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        button_rotate_l.setBackgroundResource(R.drawable.yellow_circle);
+                        vibe.cancel();
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
                 }
                 return false;
             }
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    //tcp.send("rotl");
+                    System.out.println("rotl");
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+
         });
 
         final ImageButton button_dtr = findViewById(R.id.dtr);
@@ -184,5 +302,11 @@ public class ManualMovementActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tcp.close();
     }
 }
