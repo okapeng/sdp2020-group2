8eb11ba439156b64a3a916d07b46bf2935a26dc1
sporwal98 <32335522+sporwal98@users.noreply.github.com>
2020-02-07 12:39:43 +0000

    Addeed follow as function to move

--- move.py
+++ move.py
@@ -83,3 +83,28 @@ def square(speed,time):
             back(speed,time)
             if(fr.wait_until('holding')):
                 right(speed,time)
+       
+
+    ir = ev3.InfraredSensor('in3')
+    bs = ev3.BeaconSeeker(sensor = ir,channel = 1)
+    us = ev3.UltrasonicSensor('in4')
+    us.mode = 'US-DIST-CM'
+    #rs = ev3.RemoteControl(sensor = ir, channel = 1)
+    while(True):
+        print('ir = '+str(bs.distance)+', us = '+str(us.value()))
+        #if(us.value()<500):
+            #stop()
+            #ev3.Sound.beep(args = '-l 500')
+        #if(rs.red_down or rs.red_up or rs.blue_up or rs.blue_down):
+            #stop()
+            #break
+        #else
+        if(bs.distance>30 and abs(bs.heading)<4):
+            forward(500,100)
+        elif( bs.heading>4):
+            rotl(500,100)
+        elif(bs.heading<-4):
+            rotr(500,100)
+        else:
+            continue
+
